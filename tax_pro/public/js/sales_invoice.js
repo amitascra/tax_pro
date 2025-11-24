13e4f2768c12f7c3752d80f72973330efed549eb
1byzero <55857959+1byZero@users.noreply.github.com>
2025-11-24 15:26:09 +0530

    update tax pro

--- tax_pro/public/js/sales_invoice.js
+++ tax_pro/public/js/sales_invoice.js
@@ -9,3 +9,22 @@ frappe.ui.form.on('Sales Invoice', {
 	}
 });
 
+// Trigger calculation when charge_type changes to "On Profit Margin"
+frappe.ui.form.on('Sales Taxes and Charges', {
+	charge_type: function(frm, cdt, cdn) {
+		let row = locals[cdt][cdn];
+		if (row.charge_type === 'On Profit Margin') {
+			console.log('On Profit Margin selected - triggering calculation');
+			frm.script_manager.trigger("rate", cdt, cdn);
+			frm.cscript.calculate_taxes_and_totals();
+		}
+	},
+	rate: function(frm, cdt, cdn) {
+		let row = locals[cdt][cdn];
+		if (row.charge_type === 'On Profit Margin') {
+			console.log('Tax rate changed for On Profit Margin - triggering calculation');
+			frm.cscript.calculate_taxes_and_totals();
+		}
+	}
+});
+
